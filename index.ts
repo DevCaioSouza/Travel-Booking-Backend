7679b2934743432083a1cab7cc58d01846a4b956
DevCaioSouza <caiocbahia@hotmail.com>
2023-12-11 18:11:37 -0300

    first commit - localhost connected

---
+++ index.ts
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+//config json response
+
+app.use(express.json());
+
+//solve cors
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http//localhost:3000',
+  })
+);
+
+//public
+
+app.use(express.static('public'));
+
+//Routes
+
+app.listen(5000);
